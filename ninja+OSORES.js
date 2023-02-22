d85dbc01f2f6c7b4b465b917e5e14894180709c2
Jessica Osores <jessi.osores14@gmail.com>
2023-02-22 01:19:40 -0500

    [TAREA] sensei

--- ninja+OSORES.js
+++ ninja+OSORES.js
@@ -11,21 +11,38 @@ class Ninja {
         console.log(this.name);
     }
     showStats() {
-        console.log("Nombre del ninja: " + this.name);
-        console.log("Fuerza: " + this.fuerza);
-        console.log("velocidad: " + this.velocidad);
-        console.log("Salud del Ninja: " + this.salud);
+        console.log("Nombre: " + this.name, "Fuerza: " + this.fuerza, "velocidad: " + this.velocidad, "Salud del Ninja: " + this.salud );
     }
     drinkSake() {
         this.salud += 10;
     }
 }
 
+//Clase Sensei
+class Sensei extends Ninja{
+    constructor(name){
+        super(name);
+        this.salud=200;
+        this.velocidad=10;
+        this.fuerza =10;
 
+        this.sabiduria = 10;
+    }
+    speakWisdom(){
+        console.log("Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses.");
+        this.drinkSake();
+    }
+}
 
+//Prueba para ninja
 const ninja1 = new Ninja("Hyabusa");
 ninja1.drinkSake();
 ninja1.sayName();
 ninja1.showStats();
 
-
+//Prueba sensei
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses."
+superSensei.showStats();
+// -> "Nombre: Master Splinter, Salud: 210, Velocidad: 10, Fuerza: 10"
